cfb18aa09f83dc8d1564f23d3c438c33485665e9
Di <elven.legacy@hotmail.com>
2021-11-25 23:59:12 +0300

    Настройка storybook, prettier, eslint

--- src/atoms/buttons/defaultButton/index.tsx
+++ src/atoms/buttons/defaultButton/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export interface DefaultButtonProps {}
+export interface DefaultButtonProps {
+  disabled: boolean,
+  children?: ((props: any) => React.ReactNode) | React.ReactNode | string;
+}
 
-const DefaultButton: React.FC<DefaultButtonProps> = ({}) => {
-  return <button>default</button>;
+const DefaultButton: React.FC<DefaultButtonProps> = ({disabled, children}) => {
+  return <button disabled={disabled}>{children}</button>;
 };
 
 export default DefaultButton;
