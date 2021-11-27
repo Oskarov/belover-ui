6e1eb101813083b48dc4fae3f02feac1ef1fb2b8
Di <elven.legacy@hotmail.com>
2021-11-27 22:05:39 +0300

    оборачивание компонента в forwardRef

--- src/atoms/buttons/defaultButton/index.tsx
+++ src/atoms/buttons/defaultButton/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-export interface DefaultButtonProps {
-  disabled: boolean,
-  children?: ((props: any) => React.ReactNode) | React.ReactNode | string;
-}
+/*export interface DefaultButtonProps {
+    disabled: boolean,
+    children?: ((props: any) => React.ReactNode) | React.ReactNode | string;
+}*/
 
-const DefaultButton: React.FC<DefaultButtonProps> = ({disabled, children}) => {
-  return <button disabled={disabled}>{children}</button>;
-};
+const DefaultButton = React.forwardRef<HTMLButtonElement,
+    React.ComponentPropsWithoutRef<'button'>>(({
+                                                   children,
+                                                   ...props
+                                               }, ref) => {
+    return <button type="button" ref={ref} {...props}>{children}</button>;
+});
+
+DefaultButton.displayName = 'DefaultButton';
 
 export default DefaultButton;
--- src/stories/atoms/buttons/DefaultButton.stories.tsx
+++ src/stories/atoms/buttons/DefaultButton.stories.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
-import DefaultButton, {
-  DefaultButtonProps,
-} from '../../../atoms/buttons/defaultButton';
+import DefaultButton from '../../../atoms/buttons/defaultButton';
 
 export default {
   title: 'Atoms/Buttons/DefaultButton',
   component: DefaultButton,
 } as Meta;
 
-const Template: Story<DefaultButtonProps & { disabled: boolean }> = (args) => (
-  <DefaultButton {...args}>My button</DefaultButton>
+const Template: Story = (args) => (
+  <DefaultButton {...args}>Создать запись</DefaultButton>
 );
 
 export const Default = Template.bind({});
